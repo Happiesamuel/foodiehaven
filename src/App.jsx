f80bd7cec56d8a41a0db882796deb77f5da2d7a3
Hs-the-dev <odioneghogho@gmail.com>
2024-07-18 10:05:50 +0100

    suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Home from "./pages/Home";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { GlobalStyle } from "./styles/GlobalStyle";
-import About from "./pages/About";
-import Customer from "./pages/Customer";
 import { ToogleContext } from "./context/ToogleContext";
-import Login from "./features/authLayout/Login";
-import Auth from "./pages/Auth";
-import Signup from "./features/authLayout/Signup";
-import { Toaster } from "react-hot-toast";
-import Dashboard from "./pages/Dashboard";
-import AppLayout from "./ui/AppLayout";
-import Settings from "./pages/Settings";
-import Bookmarks from "./pages/Bookmarks";
 import { MenuContext } from "./context/MenuContext";
 import { SearchResultContext } from "./context/SearchResultContext";
-import RecipeData from "./features/recipeInfo/RecipeData";
-import Cart from "./pages/Cart";
-import Order from "./pages/Order";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { Toaster } from "react-hot-toast";
 import { OrderContext } from "./context/OrderContext";
-import FinalOrder from "./pages/FinalOrder";
 import { DarkmodeContext } from "./context/DarkmodeContext";
-import PageNotFound from "./pages/PageNotFound";
-import Orders from "./pages/Orders";
-import { useEffect, useState } from "react";
 import Preloader from "./ui/Preloader";
 import ProtectedRoute from "./ui/ProtectedRoute";
-import Contact from "./pages/Contact";
+
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Customer = lazy(() => import("./pages/Customer"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Login = lazy(() => import("./features/authLayout/Login"));
+const Signup = lazy(() => import("./features/authLayout/Signup"));
+const Auth = lazy(() => import("./pages/Auth"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const AppLayout = lazy(() => import("./ui/AppLayout"));
+const Settings = lazy(() => import("./pages/Settings"));
+const Bookmarks = lazy(() => import("./pages/Bookmarks"));
+const RecipeData = lazy(() => import("./features/recipeInfo/RecipeData"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Order = lazy(() => import("./pages/Order"));
+const FinalOrder = lazy(() => import("./pages/FinalOrder"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const Orders = lazy(() => import("./pages/Orders"));
 
 function App() {
   const queryClient = new QueryClient({
@@ -37,13 +37,7 @@ function App() {
       },
     },
   });
-  const [load, setLoad] = useState(true);
-  useEffect(function () {
-    setTimeout(function () {
-      setLoad(false);
-    }, 5000);
-  }, []);
-  if (load) return <Preloader />;
+
   return (
     <QueryClientProvider client={queryClient}>
       <DarkmodeContext>
@@ -53,36 +47,38 @@ function App() {
               <SearchResultContext>
                 <GlobalStyle />
                 <BrowserRouter>
-                  <Routes>
-                    <Route index element={<Home />} />
-                    <Route path="about" element={<About />} />
-                    <Route path="customer" element={<Customer />} />
-                    <Route path="contact" element={<Contact />} />
+                  <Suspense fallback={<Preloader />}>
+                    <Routes>
+                      <Route index element={<Home />} />
+                      <Route path="about" element={<About />} />
+                      <Route path="customer" element={<Customer />} />
+                      <Route path="contact" element={<Contact />} />
 
-                    <Route index path="*" element={<PageNotFound />} />
+                      <Route index path="*" element={<PageNotFound />} />
 
-                    <Route element={<Auth />}>
-                      <Route path="login" element={<Login />} />
-                      <Route path="signup" element={<Signup />} />
-                    </Route>
-                    <Route
-                      element={
-                        <ProtectedRoute>
-                          <AppLayout />
-                        </ProtectedRoute>
-                      }
-                    >
-                      <Route path="dashboard" element={<Dashboard />} />
-                      <Route path="bookmark" element={<Bookmarks />} />
-                      <Route path="cart" element={<Cart />} />
-                      <Route path="orders" element={<Orders />} />
-                      <Route path="order" element={<Order />} />
-                      <Route path="order/:orderId" element={<FinalOrder />} />
+                      <Route element={<Auth />}>
+                        <Route path="login" element={<Login />} />
+                        <Route path="signup" element={<Signup />} />
+                      </Route>
+                      <Route
+                        element={
+                          <ProtectedRoute>
+                            <AppLayout />
+                          </ProtectedRoute>
+                        }
+                      >
+                        <Route path="dashboard" element={<Dashboard />} />
+                        <Route path="bookmark" element={<Bookmarks />} />
+                        <Route path="cart" element={<Cart />} />
+                        <Route path="orders" element={<Orders />} />
+                        <Route path="order" element={<Order />} />
+                        <Route path="order/:orderId" element={<FinalOrder />} />
 
-                      <Route element={<Settings />} path="settings" />
-                      <Route element={<RecipeData />} path="recipe/:id" />
-                    </Route>
-                  </Routes>
+                        <Route element={<Settings />} path="settings" />
+                        <Route element={<RecipeData />} path="recipe/:id" />
+                      </Route>
+                    </Routes>
+                  </Suspense>
                 </BrowserRouter>
                 <Toaster
                   toastOptions={{
